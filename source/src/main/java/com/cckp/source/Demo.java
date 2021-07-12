5f6ccfc23e793319575f9825906edcb1fc776074
lijiasheng <lijiasheng@sinobest.cn>
2021-07-12 08:35:09 +0800

    【增加】demo

---
+++ source/src/main/java/com/cckp/source/Demo.java
@@ -0,0 +1,14 @@
+package com.cckp.source;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ljs
+ * @version 1.0
+ * @description
+ * @date 2021/7/9 9:16
+ */
+public class Demo {
+
+}
---
+++ zk-client/src/main/java/com/cckp/zk/test/DEmo.java
@@ -0,0 +1,16 @@
+package com.cckp.zk.test;
+
+import java.util.HashMap;
+
+/**
+ * @author ljs
+ * @version 1.0
+ * @description
+ * @date 2021/7/9 9:21
+ */
+public class DEmo {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(null,"111");
+    }
+}
